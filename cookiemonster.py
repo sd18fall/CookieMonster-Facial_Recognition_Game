ebe52c1193eb829fdf9ed2377fce3cd08e11534d
Anya <anyaljensen@gmail.com>
2018-10-18 12:29:10 -0400

    Add player and mouth class

--- cookiemonster.py
+++ cookiemonster.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
 import random
+import pygame
 
 cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier('/home/ajensen/Downloads/haarcascade_frontalface_alt.xml') #find classifier
 kernel = np.ones((21, 21), 'uint8')
 mouth_y = [0,0] #initialize array for mouth y
 mouth_x = [0,0] #initialize array for mouth x
@@ -15,8 +15,13 @@ class Model(object):
     methods: eat
     """
     def __init__(self):
-        self.cookies = []
-        self.cookies.append(Cookies())
+        self.cookies = Cookies()
+        self.player = Player()
+    def update(self):
+        """ Update the game state (currently only tracking the cookies) """
+        self.cookies.update()
+        self.player.mouth.recognize()
+        #if mouth = cookie then cookie.eat
 
 class Cookies(object):
     """
@@ -24,23 +29,62 @@ class Cookies(object):
     methods: rando_place (init)
     """
     def __init__(self):
-        self.x= random.randint(0,400)
-        self.y= random.randint(0,400)
+        self.x= random.randint(100,500)
+        self.y= 0
         self.radius = random.randint(40,50)
+        self.velocity = 5.0
+        self.speed = 3
+    def update(self):
 
-class Person(object):
-    """
-    Controller for game
-    Attributes: x, y, mouth_open
-    """
-    def __init__(self, x, y, open):
-        self.x = x
-        self.y = y
-        self.open = mouth_open
+        if self.y<800:
+            self.y =int(self.y+ self.speed)
+        else:
+            self.y = 0
+            self.x= random.randint(100,500)
+            if self.speed<15:
+                self.speed = self.speed * 1.2
+    def eat(self, player):
+        self.y=1000
+        player.score+=1
+        self.speed = self.speed * 1.2
+        pass
 
+class Bomb(Cookies):
+    """
+    Bomb
+    """
+    def eat(self, player):
+        self.y=1000
+        player.score-=10
+        pass
+
+class Mouth(object):
+    """Creates a mouth in th model"""
+    def __init__(self, mouth_x1=0, mouth_y1=0, mouth_x2=0, mouth_y2=0):
+        self.x1=mouth_x1
+        self.y1=mouth_y1
+        self.x2 = mouth_x2
+        self.y2 = mouth_y2
+    def recognize(self):
+        ret, frame = cap.read()
+        self.face_cascade = cv2.CascadeClassifier('/home/ajensen/Downloads/haarcascade_frontalface_alt.xml')
+        self.faces = self.face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20, 30))
+        for (x, y, w, h) in self.faces:
+            self.mouth_y1 = int(y+4*h/5)
+            self.mouth_y2 = int(y+3*h/4)
+            self.mouth_x1 = int(x+w/4)
+            self.mouth_x2 = int(x+3*w/4)
+
+class Player(object):
+    """
+    Player with score and whatnot
+    """
+    def __init__(self):
+        self.score=0
+        self.mouth=Mouth()
 
 class View:
-    """blah"""
+    """Display cookies on screen """
     def __init__(self, model):
         """
         Initialize the view with a reference to the model and the specified game screen dimensions (represented as a tuple containing the width and height
@@ -54,8 +98,7 @@ class View:
         """
 
         #self.screen.fill(pygame.Color(0,0,0))
-        for cookie in self.model.cookies:
-            cv2.circle(frame,(cookie.x,cookie.y),(cookie.radius), (100,100,100))
+        pygame.circle(frame,(100,100,100),(self.model.cookies.x,self.model.cookies.y),(self.model.cookies.radius))
 
 
 if __name__ == '__main__':
@@ -68,38 +111,10 @@ if __name__ == '__main__':
 
     while True:
         # Capture frame-by-frame
-        ret, frame = cap.read()
-
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
+        pygame.init()
+        model.update()
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-        view.draw(frame)
-        cv2.imshow('frame', frame)
-        faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20, 30))
-        for (x, y, w, h) in faces:
-            #frame[y:y+h, x:x+w, :] = cv2.dilate(frame[y:y+h, x:x+w, :], kernel)
-            mouth_y = [int(y+4*h/5), int(y+3*h/4)]
-            mouth_x = [int(x+w/4), int(x+3*w/4)]
-            if (h>200):
-                mouth_open = True
-            else:
-                mouth_open = False
-
-            print(mouth_open)
-            cv2.rectangle(frame, (mouth_x[0], mouth_y[0]), (mouth_x[1], mouth_y[1]), (0, 0, 255))
-
-            # Display the resulting frame
-            cv2.imshow('frame', frame)
-            #for cookies in model
-                #if cookie is in range of box
-                    #destroy cookie
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-
-
 
 
 
